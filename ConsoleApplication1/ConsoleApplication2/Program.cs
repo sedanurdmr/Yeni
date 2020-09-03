2b4f7fba2f71e1dfb29755ed246d3497e3a83d91
Sedanur Demir <dmrsdnr1@gmail.com>
2020-09-03 16:23:41 +0300

    2.versiyon

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 5;
+            int sayi2 = 4;
+            int toplam = sayi1 + sayi2;
+            Console.Write(toplam);
+        }
+    }
+}
