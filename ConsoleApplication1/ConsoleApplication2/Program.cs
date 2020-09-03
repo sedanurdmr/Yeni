c5af9e23ad6171a1d12202f6f48fadf243fbf1c0
Sedanur Demir <dmrsdnr1@gmail.com>
2020-09-03 17:17:02 +0300

    cikarma eklendi

--- ConsoleApplication1/ConsoleApplication2/Program.cs
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -13,9 +13,12 @@ namespace ConsoleApplication2
             int sayi1 = 5;
             int sayi2 = 4;
             int toplam = sayi1 + sayi2;
+            int cikarma = sayi1; -sayi2;
             int carp = sayi1 * sayi2;
             Console.Write(toplam);
             Console.Write(carp);
+            Console.Write(cikarma);
+
 
         }
     }
