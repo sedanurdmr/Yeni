f1d9b224638191b22efea48a760dbb23198b68dd
Sedanur Demir <dmrsdnr1@gmail.com>
2020-09-03 17:18:14 +0300

    cikarma düzeltildi

--- ConsoleApplication1/ConsoleApplication2/Program.cs
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication2
             int sayi1 = 5;
             int sayi2 = 4;
             int toplam = sayi1 + sayi2;
-            int cikarma = sayi1; -sayi2;
+            int cikarma = sayi1 -sayi2;
             int carp = sayi1 * sayi2;
             Console.Write(toplam);
             Console.Write(carp);
